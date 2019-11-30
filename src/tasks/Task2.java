db1219f52f3b65daabbdb68d4a4104dc2506643f
Татьяна <t.umnova@a1-systems.com>
2019-11-30 23:57:08 +0300

    выполнено 2-е задание

--- src/tasks/Task2.java
+++ src/tasks/Task2.java
@@ -4,10 +4,7 @@ import common.Person;
 import common.Task;
 
 import java.time.Instant;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 import java.util.stream.Collectors;
 
 /*
@@ -22,7 +19,14 @@ public class Task2 implements Task {
   private static List<Person> combineAndSortWithLimit(Collection<Person> persons1,
                                                       Collection<Person> persons2,
                                                       int limit) {
-    return new ArrayList<>();
+    List<Person> all = new ArrayList<>();
+    all.addAll(persons1);
+    all.addAll(persons2);
+
+    return all.stream().
+            sorted(Comparator.comparing(Person::getCreatedAt))
+            .limit(limit)
+            .collect(Collectors.toList());
   }
 
   @Override
